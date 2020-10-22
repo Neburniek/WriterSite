944acb1678aff37d6d4f2ef65204637fc4a4d223
Neburniek <rubenpuerta89@gmail.com>
2020-10-22 10:45:31 +0200

    before adding it to netlify

--- index.js
+++ index.js
@@ -1,5 +1,60 @@
 
 
+//list of books
+
+
+const booksList = [
+  {title: "Fight Club",
+    imgURL: "https://live.staticflickr.com/2291/2320868352_c577a53c9d.jpg",
+    imgAuthor: "K嘛 / Flickr",
+    imgLink:"https://www.flickr.com/photos/kmar/2320868352/in/photolist-bp1QF-2js1E3B-2iX9Zb5-F5JP2d-KpD91P-b6myET-2iYxana-2go7vCg-4x64iA-JnzG8z-mXZpn1-2iAfb22-Ku7ATr-25G5xy-TnoBDA-87ybiz-86TQPG-fe2AwY-7adQdN-88KgQU-7aa1Ea-2a6c8ci-jvxk6-2uTjQX-26jYJKN-Q4hJsW-R8qBfK-92Jvhw-qkPrF4-27JZeDN-bqZ9Hm-8DNuSJ-7yuQFL-7cfw5Y-rm4v7S-po7qyH-S7a8BV-4KKujS-m2BKPn-8nHegF-5E8JFK-7Kwe9V-oLFx9v-611ffi-9SZK3p-dEDe3A-8nLmMh-mGUQGF-YRS7Lf-ea7oqa",
+    imgLicense: "(CC BY-NC-ND 2.0)",
+    imgLicenseURL: "https://creativecommons.org/licenses/by-nc-nd/2.0/",
+    year: 1996,
+
+  
+  }, {title:"Survivor",
+      imgURL: "https://live.staticflickr.com/2291/2320868352_c577a53c9d.jpg",
+      year: 1999}
+  
+  
+  ]
+  
+  const bookDisplay= document.getElementById("bookDisplay");
+  // for(x=0; x<booksList.length; x++){
+  //   bookDisplay.insertAdjacentHTML("beforeend",`<p>${booksList[x].title}</p>`)
+  // }
+  booksList.forEach(displayBook);
+
+  function displayBook(book){
+    bookDisplay.insertAdjacentHTML("beforeend",`
+    
+    <div>
+   
+       <figure> 
+       <a href="">
+       <h4>${book.title}</h4>
+       <img src="${book.imgURL}">
+       </a>
+       <figcaption>
+       Book publication year: ${book.year}
+       <strong>Image Source: </strong> 
+       <a class="external" href="${book.imgLink}">
+        ${book.imgAuthor}
+       </a>
+       <strong>License:</strong>
+       <a class="external" href="${book.imgLicenseURL}">
+       ${book.imgLicense}
+       </a>
+       </figcaption>
+      </figure>
+    </div>
+    
+    
+    `)
+
+  }
+
 
 // External icon after external link class external
 
@@ -41,38 +96,4 @@ if(document.getElementById("Unlock")){
   })
 };
 
-
-
-//list of books
-
-const booksList = [
-  {title: "Fight Club",
-    imgURL: "https://live.staticflickr.com/2291/2320868352_c577a53c9d.jpg",
-    year: 1996,
-  
-  }, {title:"Survivor",
-      imgURL: "https://live.staticflickr.com/2291/2320868352_c577a53c9d.jpg",
-      year: 1999}
-  
-  
-  ]
-  
-  const bookDisplay= document.getElementById("bookDisplay");
-  // for(x=0; x<booksList.length; x++){
-  //   bookDisplay.insertAdjacentHTML("beforeend",`<p>${booksList[x].title}</p>`)
-  // }
-  booksList.forEach(displayBook);
-
-  function displayBook(book){
-    bookDisplay.insertAdjacentHTML("beforeend",`
-    
-    <figure>
-      <img src="${book.imgURL}">
-      <figcaption></figcaption>
-    </figure>
-    
-    
-    `)
-
-  }
 
