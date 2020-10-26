157a669db1f60e9bf2d8b72be4caaece21884275
Neburniek <rubenpuerta89@gmail.com>
2020-10-26 11:16:28 +0100

    prueba imagen

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const fictionBookList = [
   { title: "Fight Club",
-    imgURL: "https://live.staticflickr.com/2291/2320868352_c577a53c9d.jpg",
+    imgURL: "https://competent-lamarr-8bb366.netlify.app/snuff.9df61ee8.jpg",
     imgAuthor: "K嘛 / Flickr",
     imgLink:"https://www.flickr.com/photos/kmar/2320868352/in/photolist-bp1QF-2js1E3B-2iX9Zb5-F5JP2d-KpD91P-b6myET-2iYxana-2go7vCg-4x64iA-JnzG8z-mXZpn1-2iAfb22-Ku7ATr-25G5xy-TnoBDA-87ybiz-86TQPG-fe2AwY-7adQdN-88KgQU-7aa1Ea-2a6c8ci-jvxk6-2uTjQX-26jYJKN-Q4hJsW-R8qBfK-92Jvhw-qkPrF4-27JZeDN-bqZ9Hm-8DNuSJ-7yuQFL-7cfw5Y-rm4v7S-po7qyH-S7a8BV-4KKujS-m2BKPn-8nHegF-5E8JFK-7Kwe9V-oLFx9v-611ffi-9SZK3p-dEDe3A-8nLmMh-mGUQGF-YRS7Lf-ea7oqa",
     imgLicense: "(CC BY-NC-ND 2.0)",
