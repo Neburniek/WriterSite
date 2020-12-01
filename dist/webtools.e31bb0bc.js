125563ecf8c213b8d8a99a5cc25ba5a00511d5d1
Neburniek <rubenpuerta89@gmail.com>
2020-12-01 12:26:23 +0100

    animations modified

--- dist/webtools.e31bb0bc.js
+++ dist/webtools.e31bb0bc.js
@@ -622,7 +622,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34887" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35611" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
