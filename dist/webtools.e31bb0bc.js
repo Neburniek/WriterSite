6f745ee07243f5c5706d26347b110fb5aed0bb93
Neburniek <rubenpuerta89@gmail.com>
2020-12-08 12:28:54 +0100

    logo fixed

--- dist/webtools.e31bb0bc.js
+++ dist/webtools.e31bb0bc.js
@@ -622,7 +622,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44381" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33975" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
