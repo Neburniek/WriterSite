306be6784e731fa4774ccb9cd8d8580aab87f1a7
Neburniek <rubenpuerta89@gmail.com>
2020-12-18 11:42:41 +0100

    Google and youtube test

--- dist/webtools.e31bb0bc.js
+++ dist/webtools.e31bb0bc.js
@@ -622,7 +622,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "32849" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35591" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
